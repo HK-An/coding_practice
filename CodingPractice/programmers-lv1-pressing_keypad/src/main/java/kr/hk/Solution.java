1878b4c04a2bbbc9891a2c20392376aa4344589a
me <32886925+HK-An@users.noreply.github.com>
2022-06-15 18:15:14 +0900

    [추가] 프로그래머스 lv1 - 키패드 누르기 추가

---
+++ CodingPractice/programmers-lv1-pressing_keypad/src/main/java/kr/hk/Solution.java
@@ -0,0 +1,89 @@
+package kr.hk;
+
+public class Solution {
+    private Hand leftHand = new Hand(10, "left");
+    private Hand rightHand = new Hand(12, "right");
+
+    public String solution(int[] numbers, String hand) {
+        StringBuilder stb = new StringBuilder();
+
+        for(int number : numbers) {
+            if(number == 1 || number == 4 || number == 7) {
+                stb.append("L");
+                leftHand.setPosition(number);
+            }
+            else if(number == 3 || number == 6 || number == 9) {
+                stb.append("R");
+                rightHand.setPosition(number);
+            }
+            else {
+                String properHand = findProperHand(number, hand);
+                stb.append(properHand);
+            }
+        }
+
+        return stb.toString();
+    }
+
+    String findProperHand(int number, String hand) {
+        int distanceFromLeft = calculateDistance(leftHand, number);
+        int distanceFromRight = calculateDistance(rightHand, number);
+
+        if(distanceFromLeft == distanceFromRight) {
+            if(hand.equals(rightHand.getRightOrLeftStr())) {
+                rightHand.setPosition(number);
+                return "R";
+            }
+            else {
+                leftHand.setPosition(number);
+                return "L";
+            }
+        } else if (distanceFromLeft < distanceFromRight) {
+            leftHand.setPosition(number);
+            return "L";
+        }
+        else {
+            rightHand.setPosition(number);
+            return "R";
+        }
+    }
+
+    int calculateDistance(Hand hand, int number) {
+        int position = hand.getPosition();
+
+        if (position  == 0) position = 11;
+        if (number == 0 ) number = 11;
+
+        int positionX = (position - 1) / 3;
+        int positionY = (position - 1) % 3;
+
+        int numX = (number - 1) / 3;
+        int numY = (number - 1) % 3;
+
+        int distance = Math.abs(positionX - numX) + Math.abs(positionY - numY);
+
+        return distance;
+    }
+
+}
+class Hand {
+    private int position;
+    private String rightOrLeftStr;
+
+    Hand(int position, String rightOrLeftStr) {
+        this.position = position;
+        this.rightOrLeftStr = rightOrLeftStr;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String getRightOrLeftStr() {
+        return rightOrLeftStr;
+    }
+}
---
+++ CodingPractice/programmers-lv1-pressing_keypad/src/test/java/kr/hk/PressingKeypadTest.java
@@ -0,0 +1,67 @@
+package kr.hk;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class PressingKeypadTest {
+
+    Solution s = new Solution();
+    @Test
+    public void testCalculateDistance() throws Exception {
+        Hand rightHand3 = new Hand(3, "right");
+        Hand rightHand5 = new Hand(6, "right");
+        Hand rightHand7 = new Hand(9, "right");
+        Hand rightHandOrigin = new Hand(12, "right");
+
+        Hand leftHandOrigin = new Hand(10, "left");
+
+        int answer1 = s.calculateDistance(rightHand3, 2);
+        int answer2 = s.calculateDistance(rightHand5, 2);
+        int answer3 = s.calculateDistance(rightHand7, 2);
+        int answer4 = s.calculateDistance(rightHandOrigin, 0);
+
+        int answerLeft1 = s.calculateDistance(leftHandOrigin, 0);
+
+
+        assertThat(answer1).isEqualTo(1);
+        assertThat(answer2).isEqualTo(2);
+        assertThat(answer3).isEqualTo(3);
+        assertThat(answer4).isEqualTo(1);
+
+        assertThat(answerLeft1).isEqualTo(1);
+    }
+
+    @Test
+    public void case1() throws Exception {
+        int[] numbers = {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
+        String hand = "right";
+        String expected = "LRLLLRLLRRL";
+
+        String answer = s.solution(numbers, hand);
+
+        assertThat(answer).isEqualTo(expected);
+    }
+
+    @Test
+    public void case2() throws Exception {
+        int[] numbers = {7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2};
+        String hand = "left";
+        String expected = "LRLLRRLLLRR";
+
+        String answer = s.solution(numbers, hand);
+
+        assertThat(answer).isEqualTo(expected);
+    }
+
+    @Test
+    public void case3() throws Exception {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        String hand = "right";
+        String expected = "LLRLLRLLRL";
+
+        String answer = s.solution(numbers, hand);
+
+        assertThat(answer).isEqualTo(expected);
+    }
+}

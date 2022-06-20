d801ddbe2e45b4f850ca9304ba3f691a5360647b
me <32886925+HK-An@users.noreply.github.com>
2022-06-20 14:15:21 +0900

    [추가] 프로그래머스 - lv1 - 없는 숫자 더하기 추가

---
+++ CodingPractice/programmers-lv1-sum_of_abscence_number/src/main/java/kr/hk/Solution.java
@@ -0,0 +1,30 @@
+package kr.hk;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Solution {
+    public int solution(int[] numbers) {
+        List<Integer> numberList = Arrays.stream(numbers).boxed().collect(Collectors.toList());
+        List<Integer> absenceNumberList = getAbsenceNumberList(numberList);
+
+
+        int answer = 0;
+        for(int absenceNumber : absenceNumberList) {
+            answer += absenceNumber;
+        }
+        return answer;
+    }
+
+    List<Integer> getAbsenceNumberList(List<Integer> numberList) {
+        List<Integer> criteriaNumberList = Arrays.stream(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}).boxed().collect(Collectors.toList());
+        List<Integer> absenceNumberList = new ArrayList<Integer>();
+
+        for(int criteria : criteriaNumberList) {
+            if(numberList.contains(criteria) == false) absenceNumberList.add(criteria);
+        }
+        return absenceNumberList;
+    }
+}
---
+++ CodingPractice/programmers-lv1-sum_of_abscence_number/src/test/java/kr/hk/SumOfAbscenceNumberTest.java
@@ -0,0 +1,53 @@
+package kr.hk;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class SumOfAbscenceNumberTest {
+
+    private Solution solution = new Solution();
+    private int[] numbers1 = {1,2,3,4,6,7,8,0};
+    private int[] numbers2 = {5,8,4,0,6,7,9};
+
+    @Test
+    public void testGetAbsenceNumberList() {
+        List<Integer> expected1 = new ArrayList<Integer>();
+        expected1.add(5);
+        expected1.add(9);
+        List<Integer> expected2 = new ArrayList<Integer>();
+        expected2.add(1);
+        expected2.add(2);
+        expected2.add(3);
+        List<Integer> numberList1 = Arrays.stream(numbers1).boxed().collect(Collectors.toList());
+        List<Integer> numberList2 = Arrays.stream(numbers2).boxed().collect(Collectors.toList());
+
+        List<Integer> answer1 = solution.getAbsenceNumberList(numberList1);
+        List<Integer> answer2 = solution.getAbsenceNumberList(numberList2);
+
+        assertThat(answer1).isEqualTo(expected1);
+        assertThat(answer2).isEqualTo(expected2);
+    }
+
+
+    @Test
+    public void case1() {
+        int expected = 14;
+        int answer = solution.solution(numbers1);
+
+        assertThat(answer).isEqualTo(expected);
+    }
+
+    @Test
+    public void case2() {
+        int expected = 6;
+        int answer = solution.solution(numbers2);
+
+        assertThat(answer).isEqualTo(expected);
+    }
+}

810a94635cc78cb575ff7640b3b98f316015468a
H.K-An <cohny233@gmail.com>
2022-06-25 19:48:55 +0900

    [추가] 프로그래머스 - lv1 - 약수의 합

---
+++ CodingPractice/programmers-lv1-sum_of_divisor/src/main/java/kr/hk/Solution.java
@@ -0,0 +1,32 @@
+package kr.hk;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        List<Integer> divisors = getDivisors(n);
+
+        for(int divisor : divisors) {
+            answer += divisor;
+        }
+        return answer;
+    }
+
+    List<Integer> getDivisors(int n) {
+        List<Integer> divisorList = new ArrayList<>();
+        divisorList.add(1);
+
+        for(int i = 2; i <= Math.sqrt(n); i++) {
+            if(n % i == 0) {
+                if(i != Math.sqrt(n)) divisorList.add(n/i);
+                divisorList.add(i);
+            }
+        }
+        divisorList.add(n);
+        return divisorList;
+    }
+}

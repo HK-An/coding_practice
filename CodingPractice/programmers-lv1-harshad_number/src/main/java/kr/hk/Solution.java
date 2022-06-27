f516a5903c7aef45951f12c209d8dbf3c7fee983
me <32886925+HK-An@users.noreply.github.com>
2022-06-27 16:24:38 +0900

    [추가] 프로그래머스 - lv1 - 하샤드 수

---
+++ CodingPractice/programmers-lv1-harshad_number/src/main/java/kr/hk/Solution.java
@@ -0,0 +1,17 @@
+package kr.hk;
+
+public class Solution {
+    public boolean solution(int x) {
+        char[] digitCharArray = (x+"").toCharArray();
+        int sumOfDigits = 0;
+
+        for(char digit : digitCharArray) {
+            String s = String.valueOf(digit);
+            int number = Integer.parseInt(s);
+            sumOfDigits += number;
+        }
+
+        boolean answer = x % sumOfDigits == 0 ? true : false;
+        return answer;
+    }
+}

5c31bb456f1e4fe7ad96e95b3efe16e68a40a0e9
me <32886925+HK-An@users.noreply.github.com>
2022-06-27 16:29:40 +0900

    [추가] 프로그래머스 - lv1 - 핸드폰 번호 가리기

---
+++ CodingPractice/programmers-lv1-masking_number/src/main/java/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public String solution(String phone_number) {
+        if(phone_number.length() == 4) return phone_number;
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < phone_number.length(); i++) {
+            if(i < phone_number.length() - 4) sb.append("*");
+            else sb.append(phone_number.toCharArray()[i]);
+        }
+        return sb.toString();
+    }
+}

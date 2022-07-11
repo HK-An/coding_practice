b192b09933a9f41df3e4881495690358ec7fbe75
me <32886925+HK-An@users.noreply.github.com>
2022-07-11 14:17:21 +0900

    [수정] 프로그래머스 - lv1 - 체육복 통합모듈로 이동

--- CodingPractice/programmers/src/main/java/kr/hk/lv1/PEClothes.java
+++ CodingPractice/programmers/src/main/java/kr/hk/lv1/PEClothes.java
@@ -1,15 +1,15 @@
-package kr.hk;
+package kr.hk.lv1;
 
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Solution {
+public class PEClothes {
     public int solution(int n, int[] lost, int[] reserve) {
-        int[] sortedLost = Arrays.stream(lost).sorted().toArray();
-        int[] sortedReserved = Arrays.stream(reserve).sorted().toArray();
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
 
-        Map<Integer, Integer> uniformMap = getUniformMap(n, sortedLost, sortedReserved);
+        Map<Integer, Integer> uniformMap = getUniformMap(n, lost, reserve);
         return uniformMap.keySet().size();
     }
 
@@ -23,20 +23,19 @@ public class Solution {
         for(int num : reserve) {
             uniformMap.put(num, 2);
         }
-
-        uniformMap = steal(uniformMap, lost);
-        uniformMap = borrow(uniformMap, lost);
+        stolen(uniformMap, lost);
+        borrow(uniformMap, lost);
 
         return uniformMap;
     }
 
-    private Map<Integer, Integer> borrow(Map<Integer, Integer> uniformMap, int[] lost) {
+    private void borrow(Map<Integer, Integer> uniformMap, int[] lost) {
         for(int number : lost) {
             if( number > 1) {
                 int prevNumber = number - 1;
                 int uniformPrev = uniformMap.getOrDefault(prevNumber, 0);
                 if(uniformPrev == 2) {
-                    uniformMap = borrowFrom(number, prevNumber, uniformMap);
+                    borrowFrom(number, prevNumber, uniformMap);
                     continue;
                 }
             }
@@ -44,29 +43,24 @@ public class Solution {
                 int nextNumber = number + 1;
                 int uniformNext = uniformMap.getOrDefault(nextNumber, 0);
                 if(uniformNext == 2) {
-                    uniformMap = borrowFrom(number, nextNumber, uniformMap);
+                    borrowFrom(number, nextNumber, uniformMap);
                     continue;
                 }
             }
         }
-        return uniformMap;
     }
 
-    private Map<Integer, Integer> borrowFrom(int lost, int  target, Map<Integer, Integer> uniformMap) {
+    private void borrowFrom(int lost, int  target, Map<Integer, Integer> uniformMap) {
         uniformMap.put(lost, 1);
         uniformMap.put(target, 1);
-
-        return uniformMap;
     }
 
-    private Map<Integer, Integer> steal(Map<Integer, Integer> uniformMap, int[] lost) {
+    private void stolen(Map<Integer, Integer> uniformMap, int[] lost) {
         for(int number : lost) {
             int uniform = uniformMap.get(number);
 
             if(uniform == 1) uniformMap.remove(number);
             else uniformMap.put(number, 1);
         }
-
-        return uniformMap;
     }
 }

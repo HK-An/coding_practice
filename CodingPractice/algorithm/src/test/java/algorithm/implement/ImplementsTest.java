f8b9bcc12abfa01742fdf633baad5bc474967ae0
me <32886925+HK-An@users.noreply.github.com>
2022-07-29 16:52:20 +0900

    :hammer_and_wrench: 알고리즘-테스트코드 형식 변경

--- CodingPractice/algorithm/src/test/java/algorithm/implement/ImplementsTest.java
+++ CodingPractice/algorithm/src/test/java/algorithm/implement/ImplementsTest.java
@@ -1,16 +1,16 @@
 package algorithm.implement;
 
-import algorithm.implement.Greedy;
+import algorithm.implement.mst.Prim;
 import org.junit.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-public class GreedyTest {
+public class ImplementsTest {
 
-    Greedy a = new Greedy();
 
     @Test
-    public void testChangesCase1() {
+    public void test_greedy_coin_1() {
+        Greedy a = new Greedy();
         int price = 4040;
         int paid = 5000;
         int[] expected = {1,4,1,1};
@@ -21,7 +21,8 @@ public class GreedyTest {
     }
 
     @Test
-    public void testChangesCase2() {
+    public void test_greedy_coin_2() {
+        Greedy a = new Greedy();
         int price = 97680;
         int paid = 100000;
         int[] expected = {4,3,0,2};
@@ -32,7 +33,9 @@ public class GreedyTest {
     }
 
     @Test
-    public void testChangesCase3() {
+    public void test_greedy_coin_3() {
+        Greedy a = new Greedy();
+
         int price = 0;
         int paid = 100;
         int[] expected = {0,1,0,0};
@@ -43,7 +46,9 @@ public class GreedyTest {
     }
 
     @Test
-    public void testChangesCase4() {
+    public void test_greedy_coin_4() {
+        Greedy a = new Greedy();
+
         int price = 480;
         int paid = 0;
         int[] expected = {0,0,0,0};
@@ -54,7 +59,9 @@ public class GreedyTest {
     }
 
     @Test
-    public void testTimetableCase1() {
+    public void test_greedy_timetable_1() {
+        Greedy a = new Greedy();
+
         int[][] classes = {{4,7},{1,8},{2,5},{5,9},{8,10},{9,11},{1,3},{11,14},{13,16}};
         int expected = 4;
 
--- CodingPractice/algorithm/src/test/java/algorithm/implement/dfs/DFSTest.java
+++
@@ -1,8 +0,0 @@
-package algorithm.implement.dfs;
-
-import org.junit.Test;
-
-public class DFSTest {
-
-
-}
